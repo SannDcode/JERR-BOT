f86d969e790f9b354d15bcfab9d1c0093687f19d
SannDcode <maulanaabdul1354@gmail.com>
2024-04-06 13:22:13 +0700

    First commit

---
+++ settings.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// APIKEY OPEN AI
+global.jeropenai  = "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" // ISI APIKEY LU
+
+// Api
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.my.id', // GAK USAH UBAH LOL
+	fgmods: 'https://api.fgmods.xyz'
+}
+
+// APIKEY \\
+global.APIKeys = {
+	'https://api.fgmods.xyz': 'S8mje6wW', // FREE LIMIT 100
+	'https://api.zeeoneofc.my.id': 'raPeTOyWyxfCxxB' // ISI APIKEY LU
+}
+
+global.namabot = "JER-BOTZ MD" // UBAH JADI NAMA LU
+global.namaowner = "JER" // NAMA OWNER
+global.footer_text = "©" + namabot // NAMA BOT
+global.pp_bot = fs.readFileSync("./image/allmenubot.jpg") // FOTO BOT MAX 50KB BIAR GA DELAY
+global.qris = fs.readFileSync("./image/qris.jpg") // FOTO QRIS MAX 50KB BIAR GA DELLAY
+global.register = fs.readFileSync("./image/register.jpg") // FOTO MAX 50KB BIAR GA DELLAY
+global.owner = ['6285777562859', '6287871537199'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
+// - \\
+global.sessionName = 'session' // GAK USAH UBAH
+global.prefa = ['', '!', '.', '🐦', '🐤', '🗿'] // GAK USAH UBAH
+global.sewabot = ("Gada sewa2an :V") // ISI HARGA SEWA BOT LU
+global.grubbot = (`*INI KAK LINK GRUB NYA*\n\nhttps://chat.whatsapp.com/DaFIV489FMzL1TCX4tUEa7`) // GANTI LINK GRUB BOT LU \\
+// -- \\
+// FALSE OR TRUE \\
+// TRUE = AKTIF
+// FALSE = MATI
+
+global.autoTyping = false // BEBAS
+global.welcome = true // KALO MAU AUTO WELCOME UBAH JADI true
+global.left = true // KALO MAU AUTO LEFT UBAH JADI true
+global.anticall = true // TRUE AJA
+global.autoblok212 = false // BEBAS
+global.autoread = false // BEBAS
+global.autorespon = false // BEBAS
+global.onlyprem = false // BEBAS
+global.onlygrub = false // BEBAS
+global.onlypc = false // BEBAS
+global.antispam = false // TRUE KALO MAU GA DI SPAM
+
+// - \\
+// PEMISAH \\
+global.packname = '© WHYSANN.' //sticker wm ubah
+global.author = 'TUTOR GANTI WM STICKER DEK?' //sticker wm ganti nama kalian
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
